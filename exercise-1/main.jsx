632020f69233170151abc0333ffa114a22b7770f
Daniel Snitkovskiy <Daniel@D-69-91-163-210.dhcp4.washington.edu>
2016-11-01 09:58:37 -0700

    Push ex1 + 2

--- exercise-1/main.jsx
+++ exercise-1/main.jsx
@@ -1,11 +1,28 @@
 // Main.jsx file
 
 // Create a simple component
-    // In your render function...
-        // Set variables `name` and `interest`
+var SimpleComponent = React.createClass({
+    render: function() {
+        var name = 'Daniel';
+        var interest = 'Data Science';
+        return (
+            <div>
+                <p>{name}</p>
+                <p>{interest}</p>
+            </div>
+        )
+    }
+});
 
+// In your render function...
+// Set variables `name` and `interest`
 
-        // Return div with two paragraphs
-
+// Return div with two paragraphs
 
 // Render your component in the `main` section
+ReactDOM.render(
+    <div>
+      <SimpleComponent/><SimpleComponent/><SimpleComponent/><SimpleComponent/><SimpleComponent/><SimpleComponent/>
+    </div>,
+    document.querySelector('main')
+);
--- exercise-2/main.jsx
+++ exercise-2/main.jsx
@@ -1,8 +1,18 @@
 // Main.jsx file
 
 // Create a simple component
+var SimpleDynamicComponent = React.createClass({
     // In your render function...
-
-            // Return div with two paragraphs
+    render: function() {
+        // Return div with two paragraphs
+        return (
+            <div>
+                <p>{this.props.name}</p>
+            </div>
+        )
+    }
+});
 
 // Render your component in the `main` section
+ReactDOM.render(
+    <SimpleDynamicComponent name='hello'/>, document.querySelector('body');)
